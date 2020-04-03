302da7f946c4f875b66e2f6f5c97376f5c6c1949
kakaru <kakaru1331@gmail.com>
2020-04-04 00:51:03 +0900

    Added a component which is written for api sample

---
+++ client/src/views/ApiSample.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ todos }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todos: {}
+    }
+  },
+  created () {
+    this.fetchTodos()
+  },
+  methods: {
+    async fetchTodos () {
+      const response = await this.$axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      this.todos = response.data
+    }
+  }
+}
+</script>
