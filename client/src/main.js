4350fe5c9960588de2ef07175b867f9dfe5ff6b2
kakaru <kakaru1331@gmail.com>
2020-04-04 00:48:54 +0900

    Added global property on vue prototype

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import axios from 'axios'
+
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
