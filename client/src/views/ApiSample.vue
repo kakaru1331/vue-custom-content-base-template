c47ec26b2d603c7fa246574996b9cccf78faec9c
kakaru <kakaru1331@gmail.com>
2020-04-05 04:07:03 +0900

    Fixed component intial flow to being synchronously

--- client/src/views/ApiSample.vue
+++ client/src/views/ApiSample.vue
@@ -11,13 +11,16 @@ export default {
       todos: {}
     }
   },
-  created () {
-    this.fetchTodos()
+  async created () {
+    await this.fetchTodos()
   },
   methods: {
     async fetchTodos () {
       const response = await this.$axios.get('https://jsonplaceholder.typicode.com/todos/1')
+
       this.todos = response.data
+
+      return new Promise((resolve, reject) => resolve())
     }
   }
 }
