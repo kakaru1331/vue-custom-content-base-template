a6ebee4820e1134ae0b9724c587bbdcfd2e6baa4
kakaru <kakaru1331@gmail.com>
2020-04-05 04:07:41 +0900

    Added test code by vue constructor

--- client/tests/unit/views/ApiSample.spec.js
+++ client/tests/unit/views/ApiSample.spec.js
@@ -8,21 +8,32 @@ import { expect } from 'chai'
 import ApiSample from '@/views/ApiSample.vue'
 
 describe('ApiSample.vue', () => {
-  const mock = new MockAdapter(axios)
-  Vue.prototype.$axios = axios
+  const axiosInstance = axios.create()
+  const mock = new MockAdapter(axiosInstance)
+  Vue.prototype.$axios = axiosInstance
+  const mockResponseResult = { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+
+  mock
+    .onGet('https://jsonplaceholder.typicode.com/todos/1')
+    .reply(200, mockResponseResult)
 
   it('fetch todo list from api server when component is created', async () => {
-    const mockResponseResult = { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
-
-    mock
-      .onGet('https://jsonplaceholder.typicode.com/todos/1')
-      .reply(200, mockResponseResult)
-
     const wrapper = shallowMount(ApiSample)
     await flushPromises()
-    await wrapper.vm.$nextTick()
 
     const actualResult = JSON.stringify(wrapper.vm.todos)
+    const expectResult = JSON.stringify(mockResponseResult)
+
+    expect(actualResult).to.equal(expectResult)
+  })
+
+  it('fetch todo list from api server when component is created by constructor', async () => {
+    const Constructor = Vue.extend(ApiSample)
+    const el = document.createElement('DIV')
+    const vm = new Constructor(el).$mount()
+    await flushPromises()
+
+    const actualResult = JSON.stringify(vm.todos)
     const expectResult = JSON.stringify(mockResponseResult)
 
     expect(actualResult).to.equal(expectResult)
